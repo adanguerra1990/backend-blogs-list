e7b06b700065c27d1e73c8538c747baba1ee44ab
adanguerra1990 <aj90guerra@gmail.com>
2024-03-24 13:59:42 -0400

    Test Crear Publicacion, verificar nuemro toatl de Blogs e incrementar en 1

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,7 +3,36 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 
-// const Blog = require('../models/blog')
+const Blog = require('../models/blog')
+
+const inicialBlogs = [
+    {
+        'title': 'albert einstein',
+        'author': 'walter isaacson',
+        'url': 'https://www.amazon.com/-/es/Walter-Isaacson/dp/8499080138',
+        'likes': 6,
+        'id': '65e224b02d01c08665cd4b5f'
+    },
+    {
+        'title': 'Steve Jobs',
+        'author': 'walter isaacson',
+        'url': 'https://es.wikipedia.org/wiki/Steve_Jobs_(libro)',
+        'likes': 5,
+        'id': '65e224eb2d01c08665cd4b61'
+    },
+    {
+        'title': 'Eloquent JavaScript',
+        'author': 'Marijn Haverbeke',
+        'url': 'https://eloquentjs-es.thedojo.mx/',
+        'likes': 10,
+        'id': '65e22bff41601064bf94fddf'
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(inicialBlogs)
+})
 
 test('Devuelve todos los blog en formato Json', async () => {
     await api
@@ -12,9 +41,9 @@ test('Devuelve todos los blog en formato Json', async () => {
         .expect('Content-Type', /application\/json/)
 })
 
-test('hay 4 blogs almacenados', async () => {
+test('hay 3 blogs almacenados', async () => {
     const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(4)
+    expect(response.body).toHaveLength(3)
 })
 
 test('Verifica que cada blog tenga una propiedad id', async () => {
@@ -27,6 +56,31 @@ test('Verifica que cada blog tenga una propiedad id', async () => {
     })
 })
 
+test('crea una nueva publicación de blog y verifica que el número total de blogs se incrementa en uno', async () => {
+    const blogsInicial = await Blog.find({})
+    const contadorBlogsInicial = blogsInicial.length
+
+    const newBlog = {
+        title: 'Nuevo Blog',
+        author: 'Autor de Prueba',
+        url: 'https://www.nuevoblog.com',
+        likes: 10
+    }
+
+    const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    expect(response.body.title).toBe(newBlog.title)
+    expect(response.body.author).toBe(newBlog.author)
+    expect(response.body.url).toBe(newBlog.url)
+    expect(response.body.likes).toBe(newBlog.likes)
+
+    const blogsAfterPost = await Blog.find({})
+    expect(blogsAfterPost.length).toBe(contadorBlogsInicial + 1)
+})
 
 
 afterAll(() => {
